459bd546801f6e2e672c65c4e57efa29a2858ac1
Andrés Manrique <andress0199@outlook.com>
2021-12-09 13:45:05 -0500

    images cleaned

--- main.py
+++
@@ -1,9 +0,0 @@
-from multiprocessing import Pool, freeze_support
-from itertools import repeat
-import matplotlib.image as mpimg
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, precision_score, recall_score
-from statsmodels.api import Logit
-import pandas as pd
-import glob
-import numpy as np
